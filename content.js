92547f36ea1749ee5bcd9cc0939a8a6422b4a66d
yevheniiskakun <yevhenii.skakun@gmail.com>
2021-12-14 14:15:15 +0100

    Added newtab page and its design

--- content.js
+++ content.js
@@ -1,1 +1,7 @@
-alert("Hello from your Chrome extension!")
+
+
+
+document.body.style.backgroundImage = "url('Sunny/0.png')";
+document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.backgroundSize = "100% 100%";
+
